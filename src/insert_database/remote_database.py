563b78b9d06281dcb41c51b3667371b6fede9979
Serenay Ozalp <serenay@Serenays-MacBook-Pro.local>
2021-03-02 14:40:26 +0000

    Edit data path

--- src/insert_database/remote_database.py
+++ src/insert_database/remote_database.py
@@ -6,21 +6,34 @@ from config import config
 
 engine = create_engine(f'postgresql://{config.USERNAME}:{config.PASSWORD}@{config.IP_ADDRESS}/footballer_new')
 
-data_path = '../data/collection/footballer_data_c3/'
+data_path = '/media/datalab1/Data1/serenay/footballer_project_d1/All_data/'
 files = os.listdir(data_path)
+print(files)
 
 match_file = fnmatch.filter(files, '@*.csv')
 print(match_file)
 
 
+# for file in match_file:
+#     df = pd.read_csv(file, lineterminator="\n") tweets_table = df[ ["id", "conversation_id",
+# "user_id", "created_at", "date", "timezone", "place", "tweet", "language", "hashtags", "cashtags", "day", "hour",
+# "link", "urls", "photos", "video", "thumbnail", "retweet", "nlikes", "nreplies", "nretweets", "quote_url",
+# "search", "reply_to"]]
+#     tweets_table.to_sql('tweets', con=engine, if_exists='append')
+#     print("Tweets data is inserted to the tweets table")
+
+
 for file in match_file:
-    df = pd.read_csv(file, lineterminator="\n")
+    path = data_path + file
+    df = pd.read_csv(path, lineterminator="\n")
     tweets_table = df[
         ["id", "conversation_id", "user_id", "created_at", "date", "timezone", "place", "tweet", "language", "hashtags",
          "cashtags", "day", "hour", "link", "urls", "photos", "video", "thumbnail", "retweet", "nlikes", "nreplies",
-         "nretweets", "quote_url", "search", "reply_to"]]
+         "nretweets", "quote_url"]]
+    print("-------------tweets table is created---------")
+    tweets_table.drop_duplicates(subset=['id'], inplace=True, ignore_index=True)
     tweets_table.to_sql('tweets', con=engine, if_exists='append')
-print("Tweets data is inserted to the tweets table")
+    print("-------------tweets table is written in the database---------")
 
 # for file in match_file:
 #     df = pd.read_csv(file, lineterminator="\n")
